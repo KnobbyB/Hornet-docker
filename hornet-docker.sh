3387eee2eee70307f3941db4762e58c1858081cd
KnobbyB <35898598+KnobbyB@users.noreply.github.com>
2023-03-05 07:04:07 -0800

    Update hornet-docker.sh

--- hornet-docker.sh
+++ hornet-docker.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install docker-ce
-sudo apt update
-sudo apt install docker-compose-plugin
+sudo apt install docker-ce -y
+sudo apt update && sudo apt upgrade -y
 mkdir hornet
 cd hornet
 curl -L -O https://github.com/iotaledger/node-docker-setup/releases/download/v1.0.0-rc.8/node-docker-setup_chrysalis-v1.0.0-rc.8.tar.gz
