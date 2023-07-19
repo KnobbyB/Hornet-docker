cf844c27fcd6b05b4292284305625229c6e327a2
KnobbyB <35898598+KnobbyB@users.noreply.github.com>
2023-07-19 12:06:03 -0700

    Update hornet-docker.sh

--- hornet-docker.sh
+++ hornet-docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
+
+echo insecure >> $HOME/.curlrc      //this disables the curl issue Https
+
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
