d5db22994c8aa67d7f2c14705f7beb32559ca83e
KnobbyB <35898598+KnobbyB@users.noreply.github.com>
2023-07-21 13:20:03 -0700

    Update hornet-docker.sh

--- hornet-docker.sh
+++ hornet-docker.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
-
 echo insecure >> $HOME/.curlrc      //this disables the curl issue Https
-
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -11,6 +9,6 @@ sudo apt install docker-ce -y
 sudo apt update && sudo apt upgrade -y
 mkdir hornet
 cd hornet
-curl -L -O https://github.com/iotaledger/node-docker-setup/releases/download/v1.0.0-rc.15/node-docker-setup_stardust-v1.0.0-rc.15.tar.gz
-tar -zxf node-docker-setup_stardust-v1.0.0-rc.15.tar.gz
-sudo rm node-docker-setup_stardust-v1.0.0-rc.15.tar.gz
+curl -L -O "https://github.com/iotaledger/node-docker-setup/releases/download/v1.0.0-rc.5/node-docker-setup_chrysalis-v1.0.0-rc.5.tar.gz"
+tar -zxf node-docker-setup_chrysalis-v1.0.0-rc.5.tar.gz
+sudo rm node-docker-setup_chrysalis-v1.0.0-rc.5.tar.gz
