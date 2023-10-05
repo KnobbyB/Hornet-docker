0b47090cafba73900d9478688184035366b278f2
KnobbyB <35898598+KnobbyB@users.noreply.github.com>
2023-10-04 17:44:43 -0700

    Update hornet-docker.sh

--- hornet-docker.sh
+++ hornet-docker.sh
@@ -6,7 +6,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install docker-ce -y
-sudo apt update && sudo apt upgrade -y
+sudo apt update
 mkdir hornet
 cd hornet
 curl -L -O https://github.com/iotaledger/node-docker-setup/releases/download/v1.0.1/node-docker-setup_stardust-v1.0.1.tar.gz
